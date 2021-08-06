47328b1a1166f49d89e862b35a20e0320614aaad
jiwon_ryu <happyjw3910@gmail.com>
2021-08-06 11:01:12 +0900

    싱글톤 패턴에서 특정 필드 값 변경시 문제점

--- core/src/test/java/hello/core/singletone/StatefulService.java
+++ core/src/test/java/hello/core/singletone/StatefulService.java
@@ -1,7 +1,7 @@
 package hello.core.singletone;
 
 public class StatefulService {
-    private int price; //상태를 유지하는 필드
+    private int price; //상태를 유지하는 필드 //공유되는 필드인데 클라이언트가 이 값을 변경 가능하다. //이러면 안됨
     public void order(String name,int price){
         System.out.println("name = " + name + "price = " + price);
         this.price = price; //여기가 문제!
